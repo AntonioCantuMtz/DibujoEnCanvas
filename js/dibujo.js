91b57b2d04a2cc71330b0a048feadb4a099f2c7b
Antonio Martinez <antonio_halo1@hotmail.com>
2022-09-18 17:36:46 -0500

    A la figura que ya estaba se le agrego otra pero en sentido inverso

--- js/dibujo.js
+++ js/dibujo.js
@@ -1,16 +1,31 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var xi, yf;
+var colorcito = "red", colorcito2 = "blue";
 
-dibujarLinea("pink", 10, 300, 220, 10);
-dibujarLinea("yellow", 310, 10, 10, 220);
+for(l = 0; l < lineas; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l+1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    dibujarLinea(colorcito2, yi, 0, 300, xf);
+    console.log("Linea " + l);
+}
+
+dibujarLinea(colorcito, 1, 1, 1, 299);
+dibujarLinea(colorcito, 1, 299, 299, 299);
+dibujarLinea(colorcito2, 299, 1, 1, 1);
+dibujarLinea(colorcito2, 299, 299, 299, 1);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
     lienzo.beginPath();
     lienzo.strokeStyle = color;
-    lienzo.moveTo(xinicial, yinicial);
-    lienzo.lineTo(xfinal, yfinal);
-    lienzo.stroke();
+    lienzo.moveTo(xinicial, yinicial);//dibuja el primer punto
+    lienzo.lineTo(xfinal, yfinal);//dibuja el punto final
+    lienzo.stroke();//dibuja la linea
     lienzo.closePath();
 }
-
