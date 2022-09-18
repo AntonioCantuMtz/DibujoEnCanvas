92671321b13b9a684fab8906ee7df47c578593c0
Antonio Martinez <antonio_halo1@hotmail.com>
2022-09-18 18:36:32 -0500

    Se agregaron modificaciones al codigo, se agrego un cuadro de texto y un boton

--- js/dibujo.js
+++ js/dibujo.js
@@ -1,24 +1,11 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("boton");
+boton.addEventListener("click", dibujoPorClick);//para ponerle un evento a un objeto obtenido de un html
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;//para obtener el ancho del canvas y guardarlo en una variable llamada ancho
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var xi, yf;
-var colorcito = "red", colorcito2 = "blue";
 
-for(l = 0; l < lineas; l++)
-{
-    yi = 10 * l;
-    xf = 10 * (l+1);
-    dibujarLinea(colorcito, 0, yi, xf, 300);
-    dibujarLinea(colorcito2, yi, 0, 300, xf);
-    console.log("Linea " + l);
-}
-
-dibujarLinea(colorcito, 1, 1, 1, 299);
-dibujarLinea(colorcito, 1, 299, 299, 299);
-dibujarLinea(colorcito2, 299, 1, 1, 1);
-dibujarLinea(colorcito2, 299, 299, 299, 1);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -28,4 +15,28 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
     lienzo.lineTo(xfinal, yfinal);//dibuja el punto final
     lienzo.stroke();//dibuja la linea
     lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+    var x = parseInt(texto.value);//para obtener el valor que le usuario escribe en el cuadro de texto y pasarlo al tipo INT 
+    var lineas = 30;
+    var l = 0;
+    var yi, xf;
+    var colorcito = "red", colorcito2 = "blue";
+    var espacio = ancho / lineas;
+
+    for(l = 0; l < lineas; l++)
+    {
+        yi = espacio * l;
+        xf = espacio * (l+1);
+        dibujarLinea(colorcito, 0, yi, xf, 300);
+        dibujarLinea(colorcito2, yi, 0, 300, xf);
+        console.log("Linea " + l);
+    }
+
+    dibujarLinea(colorcito, 1, 1, 1, 299);
+    dibujarLinea(colorcito, 1, 299, 299, 299);
+    dibujarLinea(colorcito2, 299, 1, 1, 1);
+    dibujarLinea(colorcito2, 299, 299, 299, 1);
 }
